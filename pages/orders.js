d18b21d7becad6e4d55e689bfee3d9488cf542cb
Tiago Dalmolin <tiagodalmolin028@gmail.com>
2023-06-27 02:15:40 -0300

    ecommerce config admin

---
+++ pages/orders.js
@@ -0,0 +1,8 @@
+import Layout from "@/components/Layout";
+
+export default function Orders(){
+    return (
+        <Layout>Página do carrinho</Layout>
+    )
+
+}
---
+++ pages/products.js
@@ -0,0 +1,8 @@
+import Layout from "@/components/Layout";
+
+export default function Products(){
+    return (
+        <Layout>Página dos Produtos</Layout>
+    )
+
+}
